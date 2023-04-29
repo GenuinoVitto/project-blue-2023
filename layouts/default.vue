1e3afb2fb72d218f83588d9db7e071980046495a
GenuinoVitto <vittogenuino@gmail.com>
2023-04-29 18:40:50 +0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <v-app id="page">
+            <div>
+                <Nuxt />
+                <slot />
+            </div>
+        </v-app>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <div>
+            <!-- Hero -->
+            
+            <!-- Who we are -->
+            <div id="introduction">
+                <h2>What is Project Blue?</h2>
+                <p>
+                    Project Blue (PB) is a youth-led nonprofit organization that focuses on raising awareness and providing sustainable solutions on ocean pollution and marine conservation. It started as a cleanup idea that turned into an organization after the realization that cleanups aren’t enough and that we have to be more sustainable. We do a mix of physical and online projects that advocate for the oceans. It was founded in the province of Ilocos last December 2020.
+                </p>
+            </div>
+            <!-- Mission -->
+            <div id="mission">
+                <h3>Mission</h3>
+                <p>
+                    To preserve the beaches of the Philippines by designing sustainable solutions and providing educational campaigns to raise awareness and knowledge on ocean pollution and marine conservation.
+                </p>
+            </div>
+            <!-- Vision -->
+            <div id="vision">
+                <h3>Vision</h3>
+                <p>
+                    To inspire others to take action and have a community that appreciates, respects, and is aware of the environment's problems.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
